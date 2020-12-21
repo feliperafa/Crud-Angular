73ccb984ee93be509e3a858177a6205ba77ee93c
feliperafa <feliperafael_barros@hotmail.com>
2020-12-21 14:40:53 -0300

    Adicionando configuração para Deploy PRODUÇÃO

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ const path = require('path');
 
 const app = express();
 
+// Inicia a aplicação pela porta configurada
+const PORT = process.env.PORT || 8080;
+
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/crud'));
+app.use(express.static(__dirname + '/dist/Crud'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/crud/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Crud/index.html'));
 });
 
-// Inicia a aplicação pela porta configurada
-app.listen(process.env.PORT || 8080);
+app.listen(PORT, () =>{
+  console.log('Servidor Iniciado na Porta ' + PORT)
+})
